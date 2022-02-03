d82628fd2aa65d3ef37ffeda66940ab4ca250987
youzi <youri@saulx.com>
2022-02-03 13:41:23 +0100

    add localstorage sync

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 import { useEffect } from "react";
-import useGlobalState from "@based/use-global-state";
+import useGlobalState, { setGlobalState } from "@based/use-global-state";
+
+let listenerMap;
+const listener = ({ key, newValue }) =>
+  setGlobalState(`__local__${key}`, parseLocalStorage(newValue));
+
+const parseLocalStorage = (value) => {
+  if (value !== null) {
+    try {
+      const obj = JSON.parse(value);
+      if (typeof obj === "object" && obj !== null && obj.__set__) {
+        return new Set(obj.__set__);
+      } else {
+        return obj;
+      }
+    } catch (e) {}
+  }
+  return value;
+};
 
 const useLocalStorage = (key, defaultValue = undefined) => {
   const [state, setState] = useGlobalState(`__local__${key}`, () => {
     try {
       const storage = localStorage.getItem(key);
-      if (storage !== null) {
-        try {
-          const obj = JSON.parse(storage);
-          if (typeof obj === "object" && obj !== null && obj.__set__) {
-            return new Set(obj.__set__);
-          } else {
-            return obj;
-          }
-        } catch (e) {
-          return storage;
-        }
-      }
+      return parseLocalStorage(storage) || defaultValue;
     } catch (e) {
       console.warn("no localStorage available");
     }
@@ -45,7 +52,30 @@ const useLocalStorage = (key, defaultValue = undefined) => {
     } catch (e) {
       console.error(e);
     }
-  }, [state]);
+  }, [key, state]);
+
+  useEffect(() => {
+    if (!listenerMap) {
+      listenerMap = new Map();
+      window.addEventListener("storage", listener);
+    }
+
+    const cnt = listenerMap.get(key) || 0;
+    listenerMap.set(key, cnt + 1);
+
+    return () => {
+      const cnt = listenerMap.get(key) || 0;
+      if (cnt > 1) {
+        listenerMap.set(key, cnt - 1);
+      } else {
+        listenerMap.delete(key);
+      }
+      if (!listenerMap.size) {
+        listenerMap = null;
+        window.removeEventListener("storage", listener);
+      }
+    };
+  }, [key]);
 
   return [state, setState];
 };
